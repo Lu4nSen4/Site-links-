04785fc3623c28e672096928ce2fae4775b260ff
Luan Costa Sena <88979382+Lu4nSen4@users.noreply.github.com>
2025-04-23 20:49:05 -0300

    Inc/

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');       // OK se o MySQL estiver local
+define('DB_NAME', 'Cursos');   // Substitua pelo nome real do banco
+define('DB_USER', 'root');         // Usuário com permissões totais no banco
+define('DB_PASS', '');           // Senha do usuário
+define('ENVIRONMENT', 'dev');         // Correto para desenvolvimento
